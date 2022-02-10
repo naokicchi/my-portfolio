836254dcb745cf43ca650e109640f04560526e22
naokicchi <nao2takahashi1@gmail.com>
2022-02-11 03:16:56 +0900

    ポートフォリオ更新

--- main.js
+++ main.js
@@ -1,114 +1,76 @@
-const hairKarte = document.getElementById("my-products-hairkarte");
-const grapes = document.getElementById("my-products-grapes");
+const tabs = document.getElementsByClassName("tab");
+const icon_img = document.getElementById("icon-image");
+const img1 = document.getElementById("first-img");
+const img2 = document.getElementById("second-img");
+const intro = document.getElementById("intro");
+const lang = document.getElementById("lang");
+const period = document.getElementById("period");
+const link_url = document.getElementById("link-url");
+const product_image = document.getElementById("product-image");
+const title = document.getElementById("title");
 
-const hairdiv = document.getElementById("hairKarte-detail");
-const grapesdiv = document.getElementById("grapes-detail");
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].onclick = function () {
+    icon_img.src = obj[i].icon_img;
+    img1.src = obj[i].img1;
+    img1.width = obj[i].width;
+    img2.src = obj[i].img2;
+    img2.width = obj[i].width;
 
-let isHairKarte = false;
-let isGrapes = false;
+    lang.textContent = obj[i].lang;
+    intro.textContent = obj[i].intro;
+    period.textContent = obj[i].period;
+    link_url.textContent = obj[i].url;
+    product_image.className = obj[i].imageClass;
+    title.textContent = obj[i].title;
+    document
+      .getElementsByClassName("is-active")[0]
+      .classList.remove("is-active");
+    this.classList.add("is-active");
+  };
+}
 
-const productDiv = document.createElement("div");
-productDiv.className = "product-detail";
-
-const introDiv = document.createElement("div");
-const unLists = document.createElement("ul");
-const list1 = document.createElement("li");
-const list2 = document.createElement("li");
-const list3 = document.createElement("li");
-const list4 = document.createElement("li");
-unLists.append(list1);
-unLists.append(list2);
-unLists.append(list3);
-unLists.append(list4);
-introDiv.append(unLists);
-
-const imgDiv = document.createElement("div");
-const img1 = document.createElement("img");
-const img2 = document.createElement("img");
-
-imgDiv.append(img1);
-imgDiv.append(img2);
-
-productDiv.append(introDiv);
-productDiv.append(imgDiv);
-
-hairKarteObj = {
-  lang: "開発言語：swift",
-  period: " 制作期間：3ヶ月",
-  intro:
-    " 紹介文：ヘアスタイル共有SNSアプリケーションです。ヘアスタイルを検索、投稿、お気に入り追加して自分だけのヘアカタログを作成！フォロー機能もあるので自分のみたいユーザーを選別できる！",
-  url: "appStoreURL : https://apps.apple.com/jp/app/hairkarte/id1565355951",
-};
-
-grapesObj = {
-  lang: "開発言語：HTML/CSS, JavaScript",
-  period: "制作期間：3日",
-  intro:
-    "紹介文：葡萄の紹介サイトです。生産数国内TOPの都道府県の葡萄の情報を記載しています。品種紹介もしているのでみてください。",
-  url: "URL : https://naokicchi.github.io/Grapeschi/Final_Ex.html",
-};
-
-const createList = function (key) {
-  switch (key) {
-    case 1:
-      list1.textContent = hairKarteObj.lang;
-      list2.textContent = hairKarteObj.period;
-      list3.textContent = hairKarteObj.intro;
-      list4.textContent = hairKarteObj.url;
-      img1.src = "images/hair1.png";
-      img2.src = "images/hair2.png";
-      introDiv.className = "hair-info";
-      imgDiv.className = "hair-img";
-
-      hairdiv.append(productDiv);
-      img1.width = 200;
-      img2.width = 200;
-      img1.height = 385.71;
-      img2.height = 385.71;
-      break;
-    case 2:
-      list1.textContent = grapesObj.lang;
-      list2.textContent = grapesObj.period;
-      list3.textContent = grapesObj.intro;
-      list4.textContent = grapesObj.url;
-      img1.src = "images/grape-img1.png";
-      img2.src = "images/grape-img2.png";
-      introDiv.className = "grape-info";
-      imgDiv.className = "grape-img";
-      grapesdiv.append(productDiv);
-
-      img1.height = 250;
-      img2.height = 250;
-      img1.width = 426.4;
-      img2.width = 426.4;
-
-      break;
-  }
-};
-
-hairKarte.onmouseover = function () {
-  if (isHairKarte !== true) {
-    createList(1);
-    isHairKarte = true;
-  }
-};
-
-hairKarte.onmouseout = function () {
-  hairdiv.removeChild(productDiv);
-  isHairKarte = false;
-};
-
-grapes.onmouseover = function () {
-  if (isGrapes !== true) {
-    createList(2);
-    isGrapes = true;
-  }
-};
-
-grapes.onmouseout = function () {
-  grapesdiv.removeChild(productDiv);
-  isGrapes = false;
-};
+const obj = [
+  {
+    title: "HairKarte",
+    lang: "開発言語：swift",
+    period: " 制作期間：3ヶ月",
+    intro:
+      " 紹介文：ヘアスタイル共有SNSアプリケーションです。ヘアスタイルを検索、投稿、お気に入り追加して自分だけのヘアカタログを作成！フォロー機能もあるので自分のみたいユーザーを選別できる！",
+    url: "appStoreURL : https://apps.apple.com/jp/app/hairkarte/id1565355951",
+    icon_img: "images/hairkarte.jpg",
+    img1: "images/hair1.png",
+    img2: "images/hair2.png",
+    imageClass: "ios-image",
+    width: 200,
+  },
+  {
+    title: "ジョブマネ",
+    lang: "開発言語：swift",
+    period: "制作期間：2週間",
+    intro:
+      "紹介文：就活生向けアプリ / 企業情報・タスク・オープンESを一括管理！就活生向けアプリです。企業情報・タスク・オープンESを一括管理可能",
+    url: "URL : https://apps.apple.com/jp/app/id1609270122",
+    icon_img: "images/jobmane.png",
+    img1: "images/jobmane-image1.png",
+    img2: "images/jobmane-image2.png",
+    imageClass: "ios-image",
+    width: 200,
+  },
+  {
+    title: "grapes!!",
+    lang: "開発言語：HTML/CSS, JavaScript",
+    period: "制作期間：5日",
+    intro:
+      "紹介文：葡萄の紹介サイトです。生産数国内TOPの都道府県の葡萄の情報を記載しています。品種紹介もしているのでみてください。",
+    url: "URL : https://naokicchi.github.io/Grapeschi/Final_Ex.html",
+    icon_img: "images/grapes.jpg",
+    img1: "images/grape-img1.png",
+    img2: "images/grape-img2.png",
+    imageClass: "web-image",
+    width: 360,
+  },
+];
 
 const images = ["images/pic2.png", "images/geek_ios.png", "images/pic4.png"];
 const secondImages = [
